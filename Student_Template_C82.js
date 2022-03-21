0799dc12e2c30c9b1dcfe4f436110ce497522197
Jason Darius_WhiteHatJr_Coder <98407304+jason8031darius@users.noreply.github.com>
2022-03-21 13:38:36 +0530

    Add files via upload

---
+++ Student_Template_C82.js
@@ -0,0 +1,61 @@
+console.log ("My first console project");
+
+            var first_name = "Ranbir";
+            var last_name = "Kapoor";
+
+
+
+            var full_name = first_name.concat(last_name);
+            console.log(full_name);
+
+/* 
+
+<----^---->
+   Concat
+
+*/
+
+var a = 10;
+var b = 5;
+  var add = a + b;
+console.log(add);
+
+/*
+
+<----^---->
+   Add..
+
+*/
+            var sub = a - b;
+            console.log(sub);
+
+/*
+
+<----^---->
+subtraction..
+
+*/
+            var multiple = a * b;
+            console.log(multiple);
+
+/*
+
+<----^---->
+ multiply...
+
+
+  */          
+
+
+var divide = a / b ;
+            console.log(divide);
+         
+/*
+
+<----^---->
+division..
+
+
+  */    
+
+
